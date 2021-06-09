5cc5cdb3a405dcaaf28ad6a5e2e3975da9847158
woong2021 <meimi101@naver.com>
2021-06-09 17:01:40 +0900

    ### 2021-06-09
    ```
    - [중급] "Vuex" 헬퍼 함수 이해 및 리팩토링
    - [중급] "Vuex" 헬퍼함수가 주는 간편함 이해
    
    ```

--- vueProject/vue-todo/src/store/store.js
+++ vueProject/vue-todo/src/store/store.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import * as getters from './getters'
+import * as mutations from './mutations'
 
 Vue.use(Vuex);
 const storage = {
@@ -19,26 +21,6 @@ export const store = new Vuex.Store({
     state:{
         todoItems: storage.fetch()
     },
-    mutations:{
-        addOneItem(state, todoItem){
-            const obj = {completed: false, item: todoItem};
-
-            localStorage.setItem(todoItem, JSON.stringify(obj));
-            state.todoItems.push(obj);
-        },
-        removeOneItem(state, payload){
-            localStorage.removeItem(payload.todoItem.item); //저장소 삭제
-            state.todoItems.splice(payload.index, 1); // 스크립트 삭제 (splice: 기존배열을 변경하여 새로운 배열은 반환, slice: 기존배열을 변경하지 않고 삭제)
-        },
-        toggleOneItem(state, payload){
-            state.todoItems[payload.index].completed = !state.todoItems[payload.index].completed;
-            // localStorage 데이터 갱신
-            localStorage.removeItem(payload.todoItem.item, payload.index);
-            localStorage.setItem(payload.todoItem.item, JSON.stringify(payload.todoItem));
-        },
-        clearAllItem(state){
-            localStorage.clear();
-            state.todoItems = [];
-        }
-    }
+    getters,
+    mutations
 });
