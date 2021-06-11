07166d3917fe115ec35b8ca2e285c715da51c497
woong2021 <meimi101@naver.com>
2021-06-11 14:45:35 +0900

    ### 2021.06.11
    ```
    - vue.js 고급 시작
    - [고급] 라우터 설치 및 라우터 구현
    - [고급] router-view를 이용한 라우팅 컴포넌트 표시
    - [고급] redirect 속성과 router-link
    - [고급] ToolBar의 라우터 링크 스타일링
    ```

--- vueAdvanced/vue-news/src/App.vue
+++ vueAdvanced/vue-news/src/App.vue
@@ -1,26 +1,24 @@
 <template>
   <div id="app">
-      hello
+      <tool-bar></tool-bar>
+      <router-view></router-view>
   </div>
 </template>
 
 <script>
+import ToolBar from './components/ToolBar.vue';
+
 export default {
-  methods: {
-    fetchData() {
-      console.log('hello');
-    }
+  components: { ToolBar },
+  comopnents: {
+    ToolBar,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body{
+  padding: 0;
+  margin: 0;
 }
 </style>
---
+++ vueAdvanced/vue-news/src/components/ToolBar.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="header">
+        <router-link to="/news">News</router-link> |
+        <router-link to="/ask">Ask</router-link> |
+        <router-link to="/jobs">Jobs</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+    .header {
+        display: flex;
+        padding: 8px;
+        background-color: #42b883;
+    }
+
+    .header .router-link-exact-active{
+        color: #35495e;
+    }
+
+    .header a{
+        color: white;
+    }
+</style>
