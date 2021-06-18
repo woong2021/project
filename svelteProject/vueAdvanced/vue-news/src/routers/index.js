5aa1691e1c3f717bbad8236f0ba0930ba313a4f3
woong2021 <meimi101@naver.com>
2021-06-18 16:23:00 +0900

    폴더명 수정1

---
+++ svelteProject/vueAdvanced/vue-news/src/routers/index.js
@@ -0,0 +1,42 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import NewsView from '../views/NewsView.vue';
+import AskView from '../views/AskView.vue';
+import JobsView from '../views/JobsView.vue';
+import UserView from '../views/UserView.vue';
+import ItemView from '../views/ItemView.vue';
+
+Vue.use(VueRouter);
+
+export const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path:'/',
+            redirect:'/news',
+        },
+        {
+            path:'/news',
+            name: 'news',
+            component: NewsView,
+        },
+        {
+            path:'/ask',
+            name:'ask',
+            component: AskView,
+        },
+        {
+            path:'/jobs',
+            name:'jobs',
+            component: JobsView,
+        },
+        {
+            path:'/user/:id',
+            component: UserView,
+        },
+        {
+            path:'/item/:id',
+            component: ItemView,
+        }
+    ]
+});
---
+++ svelteProject/vueAdvanced/vue-news/src/store/index.js
@@ -0,0 +1,30 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import mutations from './mutations.js';
+import actions from './actions.js';
+
+//vuex는 플러그인형태로 제공
+Vue.use(Vuex);
+
+export const store = new Vuex.Store({
+    state: {
+        news: [],
+        jobs: [],
+        ask : [],
+        user: {},
+        item: {},
+    },
+    getters: {
+        fetchedAsk(state) {
+            return state.ask;
+        },
+        fetchedJobs(state){
+            return state.jobs
+        },
+        fetchedNews(state){
+            return state.news
+        }
+    },
+    mutations,
+    actions,
+});
