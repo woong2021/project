42503ec000b58ac06cf825e8090e37d9acdc5653
woong2021 <meimi101@naver.com>
2021-06-18 16:09:19 +0900

    ## 2021-06-18 - spinner 브런치

---
+++ vueSpinner/vue-news/src/api/index.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+// 1. HTTP Request & Response 와 관련된 기본 설정
+const config = {
+    baseUrl: 'https://api.hnpwa.com/v0/'
+};
+
+// 2. API 함수들을 정리
+function fetchNewsList() {
+    return axios.get(`${config.baseUrl}news/1.json`);
+}
+
+function fetchAskList() {
+    return axios.get(`${config.baseUrl}ask/1.json`);
+}
+
+function fetchJobList() {
+    return axios.get(`${config.baseUrl}jobs/1.json`);
+}
+
+function fetchUserInfo(username){
+    return axios.get(`${config.baseUrl}user/${username}.json`);
+}
+
+function fetchCommentItem(id){
+    return axios.get(`${config.baseUrl}item/${id}.json`);
+}
+
+export {
+    fetchNewsList, fetchAskList, fetchJobList, fetchUserInfo, fetchCommentItem
+}
---
+++ vueSpinner/vue-news/src/store/actions.js
@@ -0,0 +1,51 @@
+import { fetchNewsList, fetchJobList, fetchAskList, fetchUserInfo, fetchCommentItem} from '../api/index.js';
+
+export default {
+    FETCH_NEWS(context){
+        fetchNewsList()
+        .then(response => {
+            context.commit('SET_NEWS', response.data);
+            return response;
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    },
+
+    FETCH_JOBS({ commit }){
+        fetchJobList()
+        .then(({ data })=>{
+            commit('SET_JOBS', data);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    },
+    FETCH_ASK({ commit }){
+        fetchAskList()
+        .then(({ data })=>{
+            commit('SET_ASK', data);
+        })
+        .catch(error => {
+            console.log(error);
+        });
+    },
+    FETCH_USER({ commit}, name){
+        fetchUserInfo(name)
+        .then(({ data }) =>{
+            commit('SET_USER', data)
+        })
+        .catch( error => {
+            console.log(error);
+        });
+    },
+    FETCH_ITEM({ commit }, id) {
+        fetchCommentItem(id)
+        .then(({ data }) => {
+            commit('SET_ITEM', data);
+        })
+        .catch( error => {
+            console.log(error)
+        });
+    },
+}
