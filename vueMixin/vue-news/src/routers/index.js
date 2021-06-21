fc68d95b78f692cd224773d60a3e9b8f6de01383
woong2021 <meimi101@naver.com>
2021-06-21 15:36:36 +0900

    ### 2021.06.21 ``` - [HOC] HOC에서 사용할 ListView 컴포넌트 구현 - [Mixin] "Mixin" 신규 프로젝트 생성 - [Mixin] "Mixin" 을 활용한 재활용 방법 구현

---
+++ vueMixin/vue-news/src/routers/index.js
@@ -0,0 +1,45 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import NewsView from '../views/NewsView.vue';
+import JobsView from '../views/JobsView.vue';
+import AskView from '../views/AskView.vue';
+import UserView from '../views/UserView.vue';
+import ItemView from '../views/ItemView.vue';
+
+Vue.use(VueRouter);
+
+export const router = new VueRouter({
+    mode: 'history',
+    routes: [
+        {
+            path:'/',
+            redirect:'/news',
+        },
+        {
+            path:'/news',
+            name: 'news',
+            component: NewsView,
+            //component: createListView('NewsView'),
+        },
+        {
+            path:'/ask',
+            name:'ask',
+            component: AskView,
+            //component: createListView('AskView'),
+        },
+        {
+            path:'/jobs',
+            name:'jobs',
+            component: JobsView,
+            //component: createListView('JobsView'),
+        },
+        {
+            path:'/user/:id',
+            component: UserView,
+        },
+        {
+            path:'/item/:id',
+            component: ItemView,
+        }
+    ]
+});
---
+++ vueMixin/vue-news/src/views/JobsView.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <list-item></list-item>  
+    </div>
+</template>
+
+<script>
+import ListItem from '../components/ListItem.vue';
+import ListMixin from '../Mixins/ListMixin.js'; 
+
+export default {
+    components: {
+        ListItem
+    },
+    mixins: [ListMixin],
+}
+</script>
